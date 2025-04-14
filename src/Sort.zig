5eaa8ed9d6888720377e0c1ba712203b06e1b5bf
nolan freeman abbott <nolan@freemanabbott.com>
2025-04-14 12:14:06 -0500

    add stubs for sort functions

---
+++ src/Sort.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+
+pub fn insertionSort(comptime ListType: type, list: *ListType) void {
+    _ = list;
+    @panic("Not Implemented");
+}
+
+pub fn selectionSort(comptime ListType: type, list: *ListType) void {
+    _ = list;
+    @panic("Not Implemented");
+}
+
+pub fn bubbleSort(comptime ListType: type, list: *ListType) void {
+    _ = list;
+    @panic("Not Implemented");
+}
+
+pub fn shellSort(comptime ListType: type, list: *ListType) void {
+    _ = list;
+    @panic("Not Implemented");
+}
+
+pub fn quickSort(comptime ListType: type, list: *ListType) void {
+    _ = list;
+    @panic("Not Implemented");
+}
+
+pub fn mergeSort(comptime ListType: type, list: *ListType) void {
+    _ = list;
+    @panic("Not Implemented");
+}
+
+pub fn heapSort(comptime ListType: type, list: *ListType) void {
+    _ = list;
+    @panic("Not Implemented");
+}
+
+pub fn radixSort(comptime ListType: type, list: *ListType) void {
+    _ = list;
+    @panic("Not Implemented");
+}
+
+pub fn bucketSort(comptime ListType: type, list: *ListType) void {
+    _ = list;
+    @panic("Not Implemented");
+}
+
+pub fn bogoSort(comptime ListType: type, list: *ListType) void {
+    _ = list;
+    @panic("Not Implemented");
+}
