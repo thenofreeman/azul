e0cb910e79cedbeb9b1547fde67417bafa6a2449
nolan freeman abbott <nolan@freemanabbott.com>
2025-04-14 13:01:49 -0500

    write preliminary insertionSort

--- src/Sort.zig
+++ src/Sort.zig
@@ -1,8 +1,30 @@
 const std = @import("std");
 
+/// Insertion Sort
+///
+/// Time Complexity Bounds
+/// -- Upper: Ο(n^2)
+/// -- Average: Θ(n^2)
+/// -- Lower: Ω(n)
 pub fn insertionSort(comptime ListType: type, list: *ListType) void {
-    _ = list;
-    @panic("Not Implemented");
+    const ValueType = list.ValueType;
+
+    var j = 0;
+    var i = 1;
+    while (i < list.size()) : (i += 1) {
+        const temp: ValueType = list.atIndex(i);
+
+        j = i;
+        while (j > 0) : (j -= 1) {
+            if (temp.compareTo(list.atIndex(j - 1)) >= 0) {
+                break;
+            }
+
+            list.setIndex(j, list.atIndex(j - 1));
+        }
+
+        list.setIndex(j, temp);
+    }
 }
 
 pub fn selectionSort(comptime ListType: type, list: *ListType) void {
