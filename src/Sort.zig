3be2f7b3e1fe001e8e3a485003b82f3130e7fec8
nolan freeman abbott <nolan@freemanabbott.com>
2025-04-14 13:24:05 -0500

    write shellsort and comments

--- src/Sort.zig
+++ src/Sort.zig
@@ -2,17 +2,20 @@ const std = @import("std");
 
 /// Insertion Sort
 ///
-/// Time Complexity Bounds
+/// ...how it works
+///
+/// Time Complexity
 /// -- Upper: Ο(n^2)
 /// -- Average: Θ(n^2)
 /// -- Lower: Ω(n)
+///
+/// Runtime of Insertion Sort is O(n + I), where I is the number of inversions performed.
 pub fn insertionSort(comptime ListType: type, list: *ListType) void {
-    const ValueType = list.ValueType;
+    var i: usize = 1;
+    var j: usize = 1;
 
-    var j = 0;
-    var i = 1;
-    while (i < list.size()) : (i += 1) {
-        const temp: ValueType = list.atIndex(i);
+    while (i < list.length()) : (i += 1) {
+        const temp = list.atIndex(i);
 
         j = i;
         while (j > 0) : (j -= 1) {
@@ -37,9 +40,37 @@ pub fn bubbleSort(comptime ListType: type, list: *ListType) void {
     @panic("Not Implemented");
 }
 
+/// Shell Sort
+///
+/// ...how it works
+///
+/// Time Complexity Bounds
+/// -- Upper: Ο()
+/// -- Average: Θ()
+/// -- Lower: Ω()
+///
+/// ...
 pub fn shellSort(comptime ListType: type, list: *ListType) void {
-    _ = list;
-    @panic("Not Implemented");
+    var i: usize = 0;
+    var j: usize = 0;
+    var gap: usize = list.length() / 2;
+
+    while (gap > 0) : (gap /= 2) {
+        while (i < list.length()) : (i += 1) {
+            const temp = list.atIndex(i);
+
+            while (j >= gap) : (j -= gap) {
+                if (temp.compareTo(list.atIndex(j - gap)) >= 0) {
+                    break;
+                }
+
+                list.setIndex(j, list.atIndex(j - gap));
+            }
+
+            list.setIndex(j, temp);
+        }
+    }
+
 }
 
 pub fn quickSort(comptime ListType: type, list: *ListType) void {
